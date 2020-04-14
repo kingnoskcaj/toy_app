1bbca2225f997a4641614073673a3e3a8193d9ff
EC2 Default User <ec2-user@ip-172-31-35-7.ec2.internal>
2020-04-14 06:36:13 +0000

    13.9 working

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'aqueous-badlands-68769.herokuapp.com'
+  host = 'fast-fortress-32327.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
